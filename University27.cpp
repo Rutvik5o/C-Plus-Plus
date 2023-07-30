504ea3bda50d04039c45f1434f2fb3dedc9cb703
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-07-30 15:46:38 +0530

    University27.cpp

--- University27.cpp
+++ University27.cpp
@@ -1,7 +1,7 @@
 /*University 2->(7)Create a class called scheme with scheme_id,
 scheme_name,outgoing_rate, and message_charge. Derive customer
 class form scheme and include cust_id, name and mobile_no data.
-Define necessary functions to read and sisplay data. Create
+Define necessary functions to read and display data. Create
 a menu driven program to read call and message information 
 for a customer and display the detail bill. */
 
